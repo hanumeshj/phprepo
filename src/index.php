2e9db7b7456c6649b916455e9d5a7963d4963886
hanumeshj <138324937+hanumeshj@users.noreply.github.com>
2023-12-02 08:14:50 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,4 +1,4 @@
  <?php echo '<p>Hello Folks, it is possible!!</p>'; ?> 
  <?php echo '<p>Welcome to PHPApp running inside Docker container!</p>'; ?> 
  <?php echo '<p>We have automated Docker image creation and deployment!!!</p>'; ?> 
- <?php echo '<p>We have automated Docker image creation from JENKINS CICD PIPELIN</p>'; ?> 
+ <?php echo '<p>We have automated Docker image creation from JENKINS  PIPELIN</p>'; ?> 
